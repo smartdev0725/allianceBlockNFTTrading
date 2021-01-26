5edd8b063e2e3b9dbf0855a696a19c3ee1381fac
smartdev0725 <cetbdb23@gmail.com>
2021-01-26 10:17:50 -0500

    token id format by generation, increase generation function

--- contracts/NFTs/LoanNFT.sol
+++ contracts/NFTs/LoanNFT.sol
@@ -4,6 +4,7 @@ pragma solidity 0.7.0;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/presets/ERC1155PresetMinterPauser.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "../utils/Strings.sol";
 
 /**
@@ -11,6 +12,11 @@ import "../utils/Strings.sol";
  * @notice NFTs that will be held by users
  */
 contract LoanNFT is ERC1155PresetMinterPauser {
+
+    using Counters for Counters.Counter;
+
+    // Keep track of loan Ids
+    Counters.Counter private _loanIdTracker;
 
     // contract URI for marketplaces
     string private _contractURI;
@@ -24,11 +30,18 @@ contract LoanNFT is ERC1155PresetMinterPauser {
     // Mapping from token ID to IPFS hash (token metadata)
     mapping(uint => string) ipfsHashes;
 
+    // Use a split bit implementation.
+    // Store the generation in the upper 128 bits..
+    uint256 constant GENERATION_MASK = uint256(uint128(~0)) << 128;
+
+    // ..and the non-fungible loan id in the lower 128
+    uint256 constant LOAN_ID_MASK = uint128(~0);
+
     /**
     * @dev Initializes the contract by setting the base URI
     */
     constructor() public ERC1155PresetMinterPauser(""){
-        _baseURI = "https://ipfs.io/ipfs/";
+        _baseURI = "ipfs://";
         _contractURI = "https://allianceblock.io/";
     }
 
@@ -60,6 +73,55 @@ contract LoanNFT is ERC1155PresetMinterPauser {
     function unpauseTokenTransfer(uint tokenId) external{
         require(hasRole(PAUSER_ROLE, _msgSender()), "ERC1155PresetMinterPauser: must have pauser role");
         transfersPaused[tokenId] = false;
+    }
+
+    /**
+     * @dev Format tokenId into generation and index
+     */
+    function formatTokenId(uint tokenId) public pure returns(uint generation, uint loanId){
+        generation = tokenId & GENERATION_MASK;
+        loanId = tokenId & LOAN_ID_MASK;
+    }
+
+    /**
+     * @dev get tokenId from generation and loanId
+     */
+    function getTokenId(uint gen, uint loanId) public pure returns(uint tokenId){
+        return gen | loanId;
+    }
+
+    /**
+     * @dev Format tokenId into generation and index
+     */
+    function getCurrentLoanId() public view returns(uint loanId){
+        return _loanIdTracker.current();
+    }
+
+    /**
+     * @dev Mint generation 0 tokens
+     */
+    function mintGen0(address to, uint amount) public{
+        uint tokenId = getTokenId(0, getCurrentLoanId());
+        mint(to, tokenId, amount, "");
+        _loanIdTracker.increment();
+    }
+
+    /**
+     * @notice increase generation of a token
+     * @dev token is burned, and new token is minted to user
+     * @dev token owner should have approvedForAll before calling this function
+     */
+    function increaseGeneration(uint tokenId, address user, uint amount) external{
+        (uint generation, uint loanId) = formatTokenId(tokenId);
+
+        // Increase generation, leave loanId same
+        uint newTokenId = getTokenId(generation++, loanId);
+
+        // Burn previous gen tokens
+        burn(user, tokenId, amount);
+
+        // Mint new generation tokens
+        mint(user, newTokenId, amount, "");
     }
     
     /**
