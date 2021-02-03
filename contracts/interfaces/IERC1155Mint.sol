a4f12b8a9d31e9203486971284b615b6152d65f2
smartdev0725 <cetbdb23@gmail.com>
2021-02-03 17:15:26 +0000

    IERC1155Mint Interface: mint ppause and unpause addition

--- contracts/interfaces/IERC1155Mint.sol
+++ contracts/interfaces/IERC1155Mint.sol
@@ -6,6 +6,8 @@ pragma solidity >=0.6.0 <0.8.0;
  * @dev Interface of the IERC1155 mint function.
  */
 interface IERC1155Mint {
-    function mint(address to, uint256 id, uint256 amount, bytes memory data) external virtual;
+    function mintGen0(address to, uint amount) external virtual;
     function safeTransferFrom(address from, address to, uint256 id, uint256 amount, bytes calldata data) external virtual;
+    function pauseTokenTransfer(uint loanId) external virtual;
+    function unpauseTokenTransfer(uint tokenId) external virtual;
 }
