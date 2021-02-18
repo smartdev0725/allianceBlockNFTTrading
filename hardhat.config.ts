e475209a0991ec14be5afb21412aa766adc2edc0
smartdev0725 <cetbdb23@gmail.com>
2021-02-18 14:52:59 -0500

    ignore types folder, remove mnemonics and forking mainnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,20 +7,18 @@ import { NetworkUserConfig } from "hardhat/types";
 import "@nomiclabs/hardhat-truffle5";
 import "hardhat-typechain";
 
-if (!process.env.MNEMONICS) throw new Error("MNEMONICS missing from .env file");
 if (!process.env.RINKEBY_PRIVKEY) throw new Error("RINKEBY_PRIVKEY missing from .env file");
 if (!process.env.MAINNET_PRIVKEY) throw new Error("MAINNET_PRIVKEY missing from .env file");
 
-const mnemonics = process.env.MNEMONICS;
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
-      forking: {
-        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
-        blockNumber: 11589707,
-      },
+      // forking: {
+      //   url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
+      //   blockNumber: 11589707,
+      // },
     },
     rinkeby: {
       url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
