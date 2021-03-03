470699e3c9074adc608f7f438a2c359714ba6a4f
smartdev0725 <cetbdb23@gmail.com>
2021-03-03 11:54:04 -0500

    create project token mock

--- contracts/mocks/ProjectToken.sol
+++ contracts/mocks/ProjectToken.sol
@@ -8,5 +8,5 @@ import "@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
  * @title Mock contract for ALBT ERC20 Token
  */
 contract LendingToken is ERC20PresetMinterPauser {
-    constructor() ERC20PresetMinterPauser("Lending Token", "LGT"){}
+    constructor() ERC20PresetMinterPauser("Project Token", "PT"){}
 }
