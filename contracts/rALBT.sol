80e758d9d730db00ef1cdb75e3f40fa858066183
smartdev0725 <cetbdb23@gmail.com>
2021-05-18 18:52:49 +0300

    Removed transfer and added multiMint to rALBT contract

--- contracts/rALBT.sol
+++ contracts/rALBT.sol
@@ -1,17 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
-contract rALBT is ERC20, Ownable {
+contract rALBT is Ownable {
 	using SafeMath for uint256;
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
 	string constant NAME = "Reputational AllianceBlock Token"; // The name of the token
 	string constant SYMBOL = "rALBT"; // The symbol of the token
+    uint256 _totalSupply;
 
-    constructor() ERC20(NAME, SYMBOL) {}
+    mapping (address => uint256) private _balances;
+
+    /**
+     * @dev Multimints tokens.
+     * @param to The addresses that will be minted tokens.
+     * @param amounts The amounts of tokens to be minted.
+     */
+    function multiMintTo(address[] memory to, uint256[] memory amounts) external onlyOwner() {
+        require(to.length == amounts.length, "Invalid length of to or amounts");
+
+        for (uint256 i = 0; i < to.length; i++) {
+            if (to[i] != address(0)) _mint(to[i], amounts[i]);
+        }
+    }
 
     /**
      * @dev Mints amountToMint tokens to the sender
@@ -27,5 +48,87 @@ contract rALBT is ERC20, Ownable {
      */
     function burnFrom(address from, uint256 amountToBurn) external onlyOwner() {
     	_burn(from, amountToBurn);
+    }
+
+    /**
+     * @dev Returns the name of the token.
+     */
+    function name() public view returns (string memory) {
+        return NAME;
+    }
+
+    /**
+     * @dev Returns the symbol of the token, usually a shorter version of the
+     * name.
+     */
+    function symbol() public view returns (string memory) {
+        return SYMBOL;
+    }
+
+    /**
+     * @dev Returns the number of decimals used to get its user representation.
+     * For example, if `decimals` equals `2`, a balance of `505` tokens should
+     * be displayed to a user as `5,05` (`505 / 10 ** 2`).
+     *
+     * Tokens usually opt for a value of 18, imitating the relationship between
+     * Ether and Wei. This is the value {ERC20} uses, unless {_setupDecimals} is
+     * called.
+     *
+     * NOTE: This information is only used for _display_ purposes: it in
+     * no way affects any of the arithmetic of the contract, including
+     * {IERC20-balanceOf} and {IERC20-transfer}.
+     */
+    function decimals() public view returns (uint8) {
+        return 18;
+    }
+
+    /**
+     * @dev See {IERC20-totalSupply}.
+     */
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    /**
+     * @dev See {IERC20-balanceOf}.
+     */
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+    /** @dev Creates `amount` tokens and assigns them to `account`, increasing
+     * the total supply.
+     *
+     * Emits a {Transfer} event with `from` set to the zero address.
+     *
+     * Requirements:
+     *
+     * - `to` cannot be the zero address.
+     */
+    function _mint(address account, uint256 amount) internal {
+        require(account != address(0), "ERC20: mint to the zero address");
+
+        _totalSupply = _totalSupply.add(amount);
+        _balances[account] = _balances[account].add(amount);
+        emit Transfer(address(0), account, amount);
+    }
+
+    /**
+     * @dev Destroys `amount` tokens from `account`, reducing the
+     * total supply.
+     *
+     * Emits a {Transfer} event with `to` set to the zero address.
+     *
+     * Requirements:
+     *
+     * - `account` cannot be the zero address.
+     * - `account` must have at least `amount` tokens.
+     */
+    function _burn(address account, uint256 amount) internal {
+        require(account != address(0), "ERC20: burn from the zero address");
+
+        _balances[account] = _balances[account].sub(amount, "ERC20: burn amount exceeds balance");
+        _totalSupply = _totalSupply.sub(amount);
+        emit Transfer(account, address(0), amount);
     }
 }
