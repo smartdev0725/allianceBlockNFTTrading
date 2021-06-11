7797a530987b133573404bab5749f905e343451a
smartdev0725 <cetbdb23@gmail.com>
2021-06-11 18:56:17 -0300

    Add lottery test

--- tests/registry/investment/checkExecuteLotteryRun.ts
+++ tests/registry/investment/checkExecuteLotteryRun.ts
@@ -3,6 +3,7 @@ import chai, { expect } from 'chai';
 import { solidity } from 'ethereum-waffle';
 import {StakingType} from "../../helpers/registryEnums";
 import {BigNumber} from "ethers";
+import {increaseTime} from "../../helpers/time";
 const { expectRevert } = require('@openzeppelin/test-helpers');
 
 chai.use(solidity);
@@ -37,13 +38,45 @@ export default async function suite() {
       );
     });
 
-    it('when execute lottery and can run lottery only if has remaining ticket  ', async function () {
+    it('when execute lottery and the state is not started should revert  ', async function () {
       const numberOfPartitions = BigNumber.from(5);
       await this.stakingContract
         .connect(this.lender1Signer)
         .stake(StakingType.STAKER_LVL_2);
       await this.registryContract.connect(this.lender1Signer).showInterestForInvestment(this.loanId, numberOfPartitions);
 
+      await expectRevert(
+        this.registryContract.executeLotteryRun(this.loanId),
+        'Can run lottery only in Started state'
+      );
+    });
+
+    it('when execute lottery and can run lottery only if has remaining ticket  ', async function () {
+      const numberOfPartitions = BigNumber.from(3000);
+      await this.stakingContract
+        .connect(this.lender1Signer)
+        .stake(StakingType.STAKER_LVL_2);
+
+      await this.registryContract.connect(this.lender1Signer).showInterestForInvestment(this.loanId, numberOfPartitions);
+
+      // Move time to 2 days
+      await increaseTime(this.deployerSigner.provider, 2 * 24 * 60 * 60); // 2 days
+
+      await this.governanceContract
+        .connect(this.superDelegatorSigner)
+        .checkCronjobs();
+
+      await this.registryContract.connect(this.lender1Signer).executeLotteryRun(this.loanId);
+
+      const ticketsRemainingAfter = await this.registryContract.ticketsRemaining(this.loanId);
+      const remainingTicketsPerAddressAfter = await this.registryContract.remainingTicketsPerAddress(this.loanId, this.lender1);
+      const ticketsWonPerAddressAfter = await this.registryContract.ticketsWonPerAddress(this.loanId, this.lender1);
+
+      expect(remainingTicketsPerAddressAfter.toNumber()).to.be.equal(0);
+      expect(ticketsRemainingAfter.toNumber()).to.be.equal(0);
+      expect(ticketsWonPerAddressAfter.toNumber()).to.be.equal(
+        numberOfPartitions.toNumber()
+      );
     });
   });
 }
