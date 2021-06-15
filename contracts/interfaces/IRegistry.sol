9142397c0672548825e1747afb3bcf602b198c59
smartdev0725 <cetbdb23@gmail.com>
2021-06-15 17:17:52 -0300

    Add interface

--- contracts/interfaces/IRegistry.sol
+++ contracts/interfaces/IRegistry.sol
@@ -5,5 +5,7 @@ pragma solidity >=0.6.0 <0.8.0;
  * @title Interface of the Registry contract.
  */
 interface IRegistry {
+    function getRequestingInterestStatus(uint256 investmentId) external view returns (bool);
+
     function startLotteryPhase(uint256 investmentId) external;
 }
