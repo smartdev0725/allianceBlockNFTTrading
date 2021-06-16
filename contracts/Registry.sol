a8a1729cb312bd94416298de9be3f2ee2b3b5260
smartdev0725 <cetbdb23@gmail.com>
2021-06-16 14:03:40 -0300

    Improve check

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -62,11 +62,10 @@ contract Registry is Initializable, Investment, OwnableUpgradeable {
         uint256 blocksLockedForReputation_,
         uint256 lotteryNumbersForImmediateTicket_
     ) external onlyOwner() {
-        require(address(rALBT) == address(0), "Cannot initialize rALBT second time");
-        require(totalTicketsPerRun == 0, "Cannot initialize second time");
-        require(rAlbtPerLotteryNumber == 0, "Cannot initialize second time");
-        require(blocksLockedForReputation == 0, "Cannot initialize second time");
-        require(lotteryNumbersForImmediateTicket == 0, "Cannot initialize second time");
+        require(reputationalAlbt != address(0), "Cannot initialize with 0 addresses");
+        require(totalTicketsPerRun_ != 0 && rAlbtPerLotteryNumber_ != 0 && blocksLockedForReputation_ != 0 && lotteryNumbersForImmediateTicket_ != 0, "Cannot initialize with 0 value");
+        require(address(rALBT) == address(0) || totalTicketsPerRun == 0 || rAlbtPerLotteryNumber == 0 || blocksLockedForReputation == 0 || lotteryNumbersForImmediateTicket == 0,
+            "Cannot initialize second time");
 
         rALBT = IERC20(reputationalAlbt);
         totalTicketsPerRun = totalTicketsPerRun_;
