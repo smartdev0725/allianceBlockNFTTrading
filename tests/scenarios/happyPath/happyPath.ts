0436c5799ce2b6c3b4021dc5711d8f88a414b351
smartdev0725 <cetbdb23@gmail.com>
2021-07-15 11:15:59 -0400

    step 4 done

--- tests/scenarios/happyPath/happyPath.ts
+++ tests/scenarios/happyPath/happyPath.ts
@@ -46,32 +46,32 @@ export default async function suite() {
     const staker1StakingAmountBefore = await this.stakingContract.getBalance(
       this.lender1
     );
-    // const staker2StakingAmountBefore = await this.stakingContract.getBalance(
-    //   this.lender2
-    // );
-    // const staker3StakingAmountBefore = await this.stakingContract.getBalance(
-    //   this.lender3
-    // );
+    const staker2StakingAmountBefore = await this.stakingContract.getBalance(
+      this.lender2
+    );
+    const staker3StakingAmountBefore = await this.stakingContract.getBalance(
+      this.lender3
+    );
 
     const amountToStake1 = (
       await this.stakingContract.stakingTypeAmounts(StakingType.STAKER_LVL_1)
     ).sub(staker1StakingAmountBefore);
-    // const amountToStake2 = (
-    //   await this.stakingContract.stakingTypeAmounts(StakingType.STAKER_LVL_2)
-    // ).sub(staker2StakingAmountBefore);
-    // const amountToStake3 = (
-    //   await this.stakingContract.stakingTypeAmounts(StakingType.STAKER_LVL_3)
-    // ).sub(staker3StakingAmountBefore);
+    const amountToStake2 = (
+      await this.stakingContract.stakingTypeAmounts(StakingType.STAKER_LVL_2)
+    ).sub(staker2StakingAmountBefore);
+    const amountToStake3 = (
+      await this.stakingContract.stakingTypeAmounts(StakingType.STAKER_LVL_3)
+    ).sub(staker3StakingAmountBefore);
 
     const staker1ALBTBalanceBefore = await this.ALBTContract.balanceOf(
       this.lender1
     );
-    // const staker2ALBTBalanceBefore = await this.ALBTContract.balanceOf(
-    //   this.lender2
-    // );
-    // const staker3ALBTBalanceBefore = await this.ALBTContract.balanceOf(
-    //   this.lender3
-    // );
+    const staker2ALBTBalanceBefore = await this.ALBTContract.balanceOf(
+      this.lender2
+    );
+    const staker3ALBTBalanceBefore = await this.ALBTContract.balanceOf(
+      this.lender3
+    );
 
     const stakingContractALBTBalanceBefore =
       await this.ALBTContract.balanceOf(this.stakingContract.address);
@@ -79,12 +79,12 @@ export default async function suite() {
     const rALBTBalanceBefore1 = await this.rALBTContract.balanceOf(
       this.lender1
     );
-    // const rALBTBalanceBefore2 = await this.rALBTContract.balanceOf(
-    //   this.lender2
-    // );
-    // const rALBTBalanceBefore3 = await this.rALBTContract.balanceOf(
-    //   this.lender3
-    // );
+    const rALBTBalanceBefore2 = await this.rALBTContract.balanceOf(
+      this.lender2
+    );
+    const rALBTBalanceBefore3 = await this.rALBTContract.balanceOf(
+      this.lender3
+    );
 
     await expect(
       this.stakingContract
@@ -94,126 +94,124 @@ export default async function suite() {
       .to.emit(this.stakingContract, 'Staked')
       .withArgs(this.lender1, amountToStake1);
 
-    // await expect(
-    //   this.stakingContract
-    //     .connect(this.lender2Signer)
-    //     .stake(StakingType.STAKER_LVL_2)
-    // )
-    //   .to.emit(this.stakingContract, 'Staked')
-    //   .withArgs(this.lender2, amountToStake2);
+    await expect(
+      this.stakingContract
+        .connect(this.lender2Signer)
+        .stake(StakingType.STAKER_LVL_2)
+    )
+      .to.emit(this.stakingContract, 'Staked')
+      .withArgs(this.lender2, amountToStake2);
 
-    // await expect(
-    //   this.stakingContract
-    //     .connect(this.lender3Signer)
-    //     .stake(StakingType.STAKER_LVL_3)
-    // )
-    //   .to.emit(this.stakingContract, 'Staked')
-    //   .withArgs(this.lender3, amountToStake3);
+    await expect(
+      this.stakingContract
+        .connect(this.lender3Signer)
+        .stake(StakingType.STAKER_LVL_3)
+    )
+      .to.emit(this.stakingContract, 'Staked')
+      .withArgs(this.lender3, amountToStake3);
 
 
     const staker1StakingAmounAfter = await this.stakingContract.getBalance(
       this.lender1
     );
-    // const staker2StakingAmounAfter = await this.stakingContract.getBalance(
-    //   this.lender2
-    // );
-    // const staker3StakingAmounAfter = await this.stakingContract.getBalance(
-    //   this.lender3
-    // );
+    const staker2StakingAmounAfter = await this.stakingContract.getBalance(
+      this.lender2
+    );
+    const staker3StakingAmounAfter = await this.stakingContract.getBalance(
+      this.lender3
+    );
     const stakedSupplyAfter = await this.stakingContract.totalSupply();
     const staker1ALBTBalanceAfter = await this.ALBTContract.balanceOf(
       this.lender1
     );
-    // const staker2ALBTBalanceAfter = await this.ALBTContract.balanceOf(
-    //   this.lender2
-    // );
-    // const staker3ALBTBalanceAfter = await this.ALBTContract.balanceOf(
-    //   this.lender3
-    // );
+    const staker2ALBTBalanceAfter = await this.ALBTContract.balanceOf(
+      this.lender2
+    );
+    const staker3ALBTBalanceAfter = await this.ALBTContract.balanceOf(
+      this.lender3
+    );
     const stakingContractALBTBalanceAfter = await this.ALBTContract.balanceOf(
       this.stakingContract.address
     );
     const rALBTBalanceAfter1 = await this.rALBTContract.balanceOf(
       this.lender1
     );
-    // const rALBTBalanceAfter2 = await this.rALBTContract.balanceOf(
-    //   this.lender2
-    // );
-    // const rALBTBalanceAfter3 = await this.rALBTContract.balanceOf(
-    //   this.lender3
-    // );
+    const rALBTBalanceAfter2 = await this.rALBTContract.balanceOf(
+      this.lender2
+    );
+    const rALBTBalanceAfter3 = await this.rALBTContract.balanceOf(
+      this.lender3
+    );
 
     const levelOfStakerAfter1 = await this.stakerMedalNFTContract.getLevelOfStaker(this.lender1);
-    // const levelOfStakerAfter2 = await this.stakerMedalNFTContract.getLevelOfStaker(this.lender2);
-    // const levelOfStakerAfter3 = await this.stakerMedalNFTContract.getLevelOfStaker(this.lender3);
+    const levelOfStakerAfter2 = await this.stakerMedalNFTContract.getLevelOfStaker(this.lender2);
+    const levelOfStakerAfter3 = await this.stakerMedalNFTContract.getLevelOfStaker(this.lender3);
 
     const balanceStaker1Medal1 = await this.stakerMedalNFTContract.balanceOf(this.lender1, 1);
     const balanceStaker1Medal2 = await this.stakerMedalNFTContract.balanceOf(this.lender1, 2);
     const balanceStaker1Medal3 = await this.stakerMedalNFTContract.balanceOf(this.lender1, 3);
 
-    // const balanceStaker2Medal1 = await this.stakerMedalNFTContract.balanceOf(this.lender2, 1);
-    // const balanceStaker2Medal2 = await this.stakerMedalNFTContract.balanceOf(this.lender2, 2);
-    // const balanceStaker2Medal3 = await this.stakerMedalNFTContract.balanceOf(this.lender2, 3);
+    const balanceStaker2Medal1 = await this.stakerMedalNFTContract.balanceOf(this.lender2, 1);
+    const balanceStaker2Medal2 = await this.stakerMedalNFTContract.balanceOf(this.lender2, 2);
+    const balanceStaker2Medal3 = await this.stakerMedalNFTContract.balanceOf(this.lender2, 3);
 
-    // const balanceStaker3Medal1 = await this.stakerMedalNFTContract.balanceOf(this.lender3, 1);
-    // const balanceStaker3Medal2 = await this.stakerMedalNFTContract.balanceOf(this.lender3, 2);
-    // const balanceStaker3Medal3 = await this.stakerMedalNFTContract.balanceOf(this.lender3, 3);
+    const balanceStaker3Medal1 = await this.stakerMedalNFTContract.balanceOf(this.lender3, 1);
+    const balanceStaker3Medal2 = await this.stakerMedalNFTContract.balanceOf(this.lender3, 2);
+    const balanceStaker3Medal3 = await this.stakerMedalNFTContract.balanceOf(this.lender3, 3);
 
     // Then
     expect(balanceStaker1Medal1.toString()).to.be.equal('1');
     expect(balanceStaker1Medal2.toString()).to.be.equal('0');
     expect(balanceStaker1Medal3.toString()).to.be.equal('0');
 
-    // expect(balanceStaker2Medal1.toString()).to.be.equal('0');
-    // expect(balanceStaker2Medal2.toString()).to.be.equal('1');
-    // expect(balanceStaker2Medal3.toString()).to.be.equal('0');
+    expect(balanceStaker2Medal1.toString()).to.be.equal('0');
+    expect(balanceStaker2Medal2.toString()).to.be.equal('1');
+    expect(balanceStaker2Medal3.toString()).to.be.equal('0');
 
-    // expect(balanceStaker3Medal1.toString()).to.be.equal('0');
-    // expect(balanceStaker3Medal2.toString()).to.be.equal('0');
-    // expect(balanceStaker3Medal3.toString()).to.be.equal('1');
+    expect(balanceStaker3Medal1.toString()).to.be.equal('0');
+    expect(balanceStaker3Medal2.toString()).to.be.equal('0');
+    expect(balanceStaker3Medal3.toString()).to.be.equal('1');
 
     expect(levelOfStakerAfter1.toString()).to.be.equal('1');
-    // expect(levelOfStakerAfter2.toString()).to.be.equal('2');
-    // expect(levelOfStakerAfter3.toString()).to.be.equal('3');
+    expect(levelOfStakerAfter2.toString()).to.be.equal('2');
+    expect(levelOfStakerAfter3.toString()).to.be.equal('3');
 
     expect(Number(staker1StakingAmounAfter)).to.be.greaterThan(
       Number(staker1StakingAmountBefore)
     );
-    // expect(Number(staker2StakingAmounAfter)).to.be.greaterThan(
-    //   Number(staker2StakingAmountBefore)
-    // );
-    // expect(Number(staker3StakingAmounAfter)).to.be.greaterThan(
-    //   Number(staker3StakingAmountBefore)
-    // );
+    expect(Number(staker2StakingAmounAfter)).to.be.greaterThan(
+      Number(staker2StakingAmountBefore)
+    );
+    expect(Number(staker3StakingAmounAfter)).to.be.greaterThan(
+      Number(staker3StakingAmountBefore)
+    );
 
     expect(Number(staker1ALBTBalanceAfter)).to.be.equal(
       Number(staker1ALBTBalanceBefore.sub(amountToStake1))
     );
-    // expect(Number(staker2ALBTBalanceAfter)).to.be.equal(
-    //   Number(staker2ALBTBalanceBefore.sub(amountToStake2))
-    // );
-    // expect(Number(staker3ALBTBalanceAfter)).to.be.equal(
-    //   Number(staker3ALBTBalanceBefore.sub(amountToStake3))
-    // );
+    expect(Number(staker2ALBTBalanceAfter)).to.be.equal(
+      Number(staker2ALBTBalanceBefore.sub(amountToStake2))
+    );
+    expect(Number(staker3ALBTBalanceAfter)).to.be.equal(
+      Number(staker3ALBTBalanceBefore.sub(amountToStake3))
+    );
 
     expect(Number(stakingContractALBTBalanceAfter)).to.be.equal(
-      Number(stakingContractALBTBalanceBefore.add(amountToStake1))
-      // Number(stakingContractALBTBalanceBefore.add(amountToStake1).add(amountToStake2).add(amountToStake3))
+      Number(stakingContractALBTBalanceBefore.add(amountToStake1).add(amountToStake2).add(amountToStake3))
     );
 
     expect(Number(stakedSupplyAfter)).to.be.equal(
-      Number(stakedSupplyBefore.add(amountToStake1))
-      // Number(stakedSupplyBefore.add(amountToStake1).add(amountToStake2).add(amountToStake3))
+      Number(stakedSupplyBefore.add(amountToStake1).add(amountToStake2).add(amountToStake3))
     );
     expect(Number(rALBTBalanceAfter1)).to.be.greaterThan(
       Number(rALBTBalanceBefore1)
     );
-    // expect(Number(rALBTBalanceAfter2)).to.be.greaterThan(
-    //   Number(rALBTBalanceBefore2)
-    // );
-    // expect(Number(rALBTBalanceAfter3)).to.be.greaterThan(
-    //   Number(rALBTBalanceBefore3)
-    // );
+    expect(Number(rALBTBalanceAfter2)).to.be.greaterThan(
+      Number(rALBTBalanceBefore2)
+    );
+    expect(Number(rALBTBalanceAfter3)).to.be.greaterThan(
+      Number(rALBTBalanceBefore3)
+    );
 
     //4) Funders declare their intention to buy a partition (effectively depositing their funds)
     // Given
@@ -223,12 +221,12 @@ export default async function suite() {
     );
     const initLenderLendingTokenBalance1 =
       await this.lendingTokenContract.balanceOf(this.lender1);
-    // const initLenderLendingTokenBalance2 =
-    //   await this.lendingTokenContract.balanceOf(this.lender2);
-    // const initLenderLendingTokenBalance3 =
-    //   await this.lendingTokenContract.balanceOf(this.lender3);
-    // const initLenderLendingTokenBalance4 =
-    //   await this.lendingTokenContract.balanceOf(this.lender4);
+    const initLenderLendingTokenBalance2 =
+      await this.lendingTokenContract.balanceOf(this.lender2);
+    const initLenderLendingTokenBalance3 =
+      await this.lendingTokenContract.balanceOf(this.lender3);
+    const initLenderLendingTokenBalance4 =
+      await this.lendingTokenContract.balanceOf(this.lender4);
     const initEscrowLendingTokenBalance =
       await this.lendingTokenContract.balanceOf(this.escrowContract.address);
 
@@ -236,29 +234,41 @@ export default async function suite() {
       .connect(this.lender1Signer)
       .showInterestForInvestment(this.investmentId, numberOfPartitions);
 
-    // await this.registryContract
-    //   .connect(this.lender2Signer)
-    //   .showInterestForInvestment(this.investmentId, numberOfPartitions);
+    await this.registryContract
+      .connect(this.lender2Signer)
+      .showInterestForInvestment(this.investmentId, numberOfPartitions);
 
-    // await this.registryContract
-    //   .connect(this.lender3Signer)
-    //   .showInterestForInvestment(this.investmentId, numberOfPartitions);
+    await this.registryContract
+      .connect(this.lender3Signer)
+      .showInterestForInvestment(this.investmentId, numberOfPartitions);
     
     //simulates lender4 has rALBT from other methods besides staking
     //GET rALBT HERE!!
     const addressZero = '0x0000000000000000000000000000000000000000';
+    const actions = [
+      {
+        account: this.lender4,
+        actionName: 'Wallet Connect',
+        answer: 'Yes',
+        referralId: '0',
+      },
+    ];
+
+    // Given
     const reputationalAlbtRewardsPerLevel = [
       ethers.utils.parseEther('500').toString(),
       ethers.utils.parseEther('500').toString(),
       ethers.utils.parseEther('500').toString(),
       ethers.utils.parseEther('500').toString(),
     ];
+
     const reputationalAlbtRewardsPerLevelAfterFirstTime = [
       ethers.utils.parseEther('10').toString(),
       ethers.utils.parseEther('10').toString(),
       ethers.utils.parseEther('10').toString(),
       ethers.utils.parseEther('10').toString(),
     ];
+
     await this.actionVerifierContract
       .connect(this.deployerSigner)
       .importAction(
@@ -268,14 +278,7 @@ export default async function suite() {
         2,
         addressZero,
       );
-    const actions = [
-      {
-        account: this.lender4,
-        actionName: 'Wallet Connect',
-        answer: 'Yes',
-        referralId: '0',
-      },
-    ];
+
     let signature = await getSignature(
       'Wallet Connect',
       'Yes',
@@ -284,39 +287,33 @@ export default async function suite() {
       this.actionVerifierContract.address,
       web3
     );
+
     const signatures = [signature];
-    const rALBTBalanceBefore = await this.rALBTContract.balanceOf(
-      this.lender4
-    );
+
     await this.actionVerifierContract
-      .connect(this.lender1Signer)
+      .connect(this.lender3Signer)
       .provideRewardsForActions(actions, signatures);
 
-    // Move time to 1 day
-    await increaseTime(this.deployerSigner.provider, 1 * 24 * 60 * 60); // 1 day
+    for (let i = 0; i < 50; i++) {
+      await increaseTime(this.deployerSigner.provider, 1 * 24 * 60 * 60); // 1 day
+  
+      await this.actionVerifierContract
+        .connect(this.lender3Signer)
+        .provideRewardsForActions(actions, signatures);
+    }
 
-    // Provide reward second time for Wallet Connect Action
-    await this.actionVerifierContract
-      .connect(this.lender1Signer)
-      .provideRewardsForActions(actions, signatures);
-
-    const rALBTBalanceAfter = await this.rALBTContract.balanceOf(
-      this.lender4
-    );
-    console.log(`(rALBTBalanceBefore)`, parseFloat(rALBTBalanceBefore))
-    console.log(`(rALBTBalanceAfter)`, parseFloat(rALBTBalanceAfter))
-    // await this.registryContract
-    //   .connect(this.lender4Signer)
-    //   .showInterestForInvestment(this.investmentId, numberOfPartitions);
+    await this.registryContract
+      .connect(this.lender4Signer)
+      .showInterestForInvestment(this.investmentId, numberOfPartitions);
 
     const lenderLendingTokenBalanceAfter1 =
       await this.lendingTokenContract.balanceOf(this.lender1);
-    // const lenderLendingTokenBalanceAfter2 =
-    //   await this.lendingTokenContract.balanceOf(this.lender2);
-    // const lenderLendingTokenBalanceAfter3 =
-    //   await this.lendingTokenContract.balanceOf(this.lender3);
-    // const lenderLendingTokenBalanceAfter4 =
-    //   await this.lendingTokenContract.balanceOf(this.lender4);
+    const lenderLendingTokenBalanceAfter2 =
+      await this.lendingTokenContract.balanceOf(this.lender2);
+    const lenderLendingTokenBalanceAfter3 =
+      await this.lendingTokenContract.balanceOf(this.lender3);
+    const lenderLendingTokenBalanceAfter4 =
+      await this.lendingTokenContract.balanceOf(this.lender4);
     const escrowLendingTokenBalanceAfter =
       await this.lendingTokenContract.balanceOf(this.escrowContract.address);
 
@@ -324,20 +321,24 @@ export default async function suite() {
       initLenderLendingTokenBalance1.sub(amountOfLendingTokens)
     );
 
-    // expect(lenderLendingTokenBalanceAfter2).to.be.equal(
-    //   initLenderLendingTokenBalance2.sub(amountOfLendingTokens)
-    // );
-
-    // expect(lenderLendingTokenBalanceAfter3).to.be.equal(
-    //   initLenderLendingTokenBalance3.sub(amountOfLendingTokens)
-    // );
-
-    // expect(lenderLendingTokenBalanceAfter4).to.be.equal(
-    //   initLenderLendingTokenBalance4.sub(amountOfLendingTokens)
-    // );
-    expect(escrowLendingTokenBalanceAfter).to.be.equal(
-      initEscrowLendingTokenBalance.add(amountOfLendingTokens)
+    expect(lenderLendingTokenBalanceAfter2).to.be.equal(
+      initLenderLendingTokenBalance2.sub(amountOfLendingTokens)
     );
+
+    expect(lenderLendingTokenBalanceAfter3).to.be.equal(
+      initLenderLendingTokenBalance3.sub(amountOfLendingTokens)
+    );
+
+    expect(lenderLendingTokenBalanceAfter4).to.be.equal(
+      initLenderLendingTokenBalance4.sub(amountOfLendingTokens)
+    );
+
+    expect(escrowLendingTokenBalanceAfter).to.be.equal(
+      initEscrowLendingTokenBalance.add(amountOfLendingTokens).add(amountOfLendingTokens).add(amountOfLendingTokens).add(amountOfLendingTokens)
+    );
+
+    //5) The lottery is run when all the partitions have been covered
+    
 
   });
 }
