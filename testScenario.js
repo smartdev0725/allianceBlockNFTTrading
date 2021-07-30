f709655fb22777bf7564be84a06a16779856054c
smartdev0725 <cetbdb23@gmail.com>
2021-07-30 10:41:54 -0400

    updated testScenario.js

--- testScenario.js
+++ testScenario.js
@@ -5,5 +5,14 @@ const execSync = require('child_process').execSync;
 const folder = process.argv[2];
 const file = process.argv[3] || 'index.spec.ts';
 
+if (!folder) {
+  console.error("Invalid arguments.");
+  console.error("Input the name of the folder you want to run, and optionally the entry point if it's not index.spec.ts")
+  console.error("Correct input: yarn test:scenario happyPath [index.spec.ts]")
+  process.exit();
+}
+
 console.log(`hardhat test ./tests/scenarios/${folder}/${file}`);
-execSync(`hardhat test ./tests/scenarios/${folder}/${file}`, {stdio:[0, 1, 2]});
+execSync(`hardhat test ./tests/scenarios/${folder}/${file}`, {
+  stdio: [0, 1, 2],
+});
