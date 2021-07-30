8d8260d198c50cb094d2a9e7f5eb9b274874ac88
smartdev0725 <cetbdb23@gmail.com>
2021-07-30 09:49:54 -0400

    Uncomment contractSizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -98,11 +98,11 @@ const config: HardhatUserConfig = {
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY || '',
   },
-  // contractSizer: {
-  //   alphaSort: true,
-  //   runOnCompile: true,
-  //   disambiguatePaths: false,
-  // },
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: true,
+    disambiguatePaths: false,
+  },
 };
 
 export default config;
