1a98b5acf784fa6be65a6cdd95733b1f720bd10e
smartdev0725 <cetbdb23@gmail.com>
2021-08-26 11:17:38 -0400

    add option to run all scenarios

--- testScenario.js
+++ testScenario.js
@@ -1,18 +1,33 @@
 'use strict';
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-var-requires */
-const execSync = require('child_process').execSync;
-const folder = process.argv[2];
+const {execSync} = require('child_process');
+const {readdirSync} = require('fs');
+let folder = process.argv[2];
 const file = process.argv[3] || 'index.spec.ts';
 
 if (!folder) {
-  console.error("Invalid arguments.");
-  console.error("Input the name of the folder you want to run, and optionally the entry point if it's not index.spec.ts")
-  console.error("Correct input: yarn test:scenario happyPath [index.spec.ts]")
+  console.error('Invalid arguments.');
+  console.error(
+    "Input the name of the folder you want to run, and optionally the entry point if it's not index.spec.ts"
+  );
+  console.error('Correct input: yarn test:scenario happyPath [index.spec.ts]');
   process.exit();
 }
 
-console.log(`hardhat test ./tests/scenarios/${folder}/${file}`);
-execSync(`hardhat test ./tests/scenarios/${folder}/${file}`, {
-  stdio: [0, 1, 2],
+let commandString = `yarn test ./tests/scenarios/${folder}/${file}`;
+
+if (folder.toLowerCase() === 'all' || folder === '*') {
+  const dirs =  readdirSync(require('path').resolve(__dirname, 'tests', 'scenarios'), { withFileTypes: true })
+  .filter(dirent => dirent.isDirectory())
+  .map(dirent => dirent.name)
+  
+  commandString = `yarn test `;
+  dirs.forEach( dir => {
+    commandString += `./tests/scenarios/${dir}/index.spec.ts `;
+  })
+}
+
+execSync(commandString, {
+  stdio: [0, 1, 2]
 });
